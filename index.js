11d3a56b0ba8390630591a76b5cf6a0cffbcd2f9
margari25 <budryte.m@mail.ru>
2022-08-01 21:30:19 +0300

    20220801

--- index.js
+++ index.js
@@ -15,4 +15,8 @@ app.get('/test2', (req, res) => res.render('test2'));
 
 app.get('/', (req, res) => res.render('index'));
 
+const arr = ['Foo', 'Bar', 'Baz'];
+
+app.get('/db', (req, res) => render('db', {data:arr}));
+
 app.listen(port, () => console.log(`Starting sever on port ${port}`));
